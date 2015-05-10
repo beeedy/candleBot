71ba3038606a22498050bb6f432496a47afe4294
beeedy <brodbones@gmail.com>
2015-05-09 21:12:00 -0500

    Whisker code fully commented
    at this point, whisker code is believed to be finished and will not
    require any further modification

--- PIC firmware/whiskers.c
+++ PIC firmware/whiskers.c
@@ -1,11 +1,22 @@
+/*                      _    _ _     _     _
+ *                     | |  | | |   (_)   | |
+ *                     | |  | | |__  _ ___| | _____ _ __ ___
+ *                     | |/\| | '_ \| / __| |/ / _ \ '__/ __|
+ *                     \  /\  / | | | \__ \   <  __/ |  \__ \
+ *                      \/  \/|_| |_|_|___/_|\_\___|_|  |___/
+ *
+ * File:   whiskers.c
+ * Author: Broderick Carlin
+ */
+
 #include "whiskers.h"
 
-#define whisker_R PORTJbits.RJ5
-#define whisker_L PORTJbits.RJ6
+#define whisker_R PORTJbits.RJ5 // The right whisker is connected to pin RJ5
+#define whisker_L PORTJbits.RJ6 // The left whisker is connected to pin RJ6
 
-void whiskers_Init()
+void whiskers_init()
 {
-    PADCFG1bits.RJPU = 1;   //enable pullups on port J
+    PADCFG1bits.RJPU = 1;       // Enable pullups on port J
 
     whisker_R = 0;
     whisker_L = 0;
@@ -16,10 +27,14 @@ void whiskers_Init()
 
 char whiskers_pollLeft()
 {
-    return (whisker_L ^ 1);
+    return (whisker_L ^ 1);     // The whisker value is XOR'd to invert the LSB
+                                //      0x00 : whisker not being pressed
+                                //      0x01 : whisker being pressed
 }
 
 char whiskers_pollRight()
 {
-    return (whisker_R ^ 1);
+    return (whisker_R ^ 1);     // The whisker value is XOR'd to invert the LSB
+                                //      0x00 : whisker not being pressed
+                                //      0x01 : whisker being pressed
 }
--- PIC firmware/whiskers.h
+++ PIC firmware/whiskers.h
@@ -1,25 +1,53 @@
-/* 
- * File:   whiskers.h
- * Author: broderickcarlin
+/*                      _    _ _     _     _                 
+ *                     | |  | | |   (_)   | |                
+ *                     | |  | | |__  _ ___| | _____ _ __ ___ 
+ *                     | |/\| | '_ \| / __| |/ / _ \ '__/ __|
+ *                     \  /\  / | | | \__ \   <  __/ |  \__ \
+ *                      \/  \/|_| |_|_|___/_|\_\___|_|  |___/
  *
- * Created on April 13, 2015, 7:29 AM
+ * File:   whiskers.h
+ * Author: Broderick Carlin
+ *
+ * This file is responsible for returning the values seen by the two whiskers
+ * that are mounted on the front two corners of the robot's chassis. These
+ * whiskers are used as a secondary method of object avoidance that relies on
+ * physical contact with objects. Upon collision with an object, the functions
+ * provided can be used to control the control flow to compensate for the error.
+ * It is the hope that these whiskers are never used, however they will act be
+ * active at all times as a double check against other less robust object
+ * avoidance methods. The whiskers are a polled I/O and are not interrupt driven
+ * this was done to save external interrupt capabilities for more time critical
+ * tasks such as drive train encoders.
+ *
+ * -------------------------------Function List---------------------------------
+ * void whiskers_init()
+ * char whiskers_pollLeft()
+ * char whiskers_pollRight()
+ *
+ *
+ * ---------------------------Function Descriptions-----------------------------
+ * void whiskers_init()
+ *      This function is responsible for intializing the pins used by the two
+ *      whiskers as well as configuring internal pull up resistors for the
+ *      inputs. This function must be called before any other functions in this
+ *      file can be reliably used. Nothing is returned by this function.
+ *
+ * char whiskers_pollLeft()
+ * char whiskers_pollRight()
+ *      These function returns a 0x00 or 0x01 to signify if the corresponding
+ *      whisker is currently being pressed. A return value of 0x00 corresponds
+ *      to the whisker NOT being pressed, while a return value of 0x01
+ *      corresponds to the whisker currently being pressed.
  */
+
 #include <xc.h>
 
 #ifndef WHISKERS_H
 #define	WHISKERS_H
 
-#ifdef	__cplusplus
-extern "C" {
-#endif
-
-    void whiskers_Init();
+    void whiskers_init();
     char whiskers_pollLeft();
     char whiskers_pollRight();
 
-#ifdef	__cplusplus
-}
 #endif
-
-#endif	/* WHISKERS_H */
 
