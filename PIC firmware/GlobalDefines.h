db4b25b370eb8cff55341f75fdc8713158cdfd66
beeedy <brodbones@gmail.com>
2015-05-09 23:29:46 -0500

    Added licensing

--- PIC firmware/GlobalDefines.h
+++ PIC firmware/GlobalDefines.h
@@ -1,17 +1,48 @@
-/* 
+/*                         _____ _       _           _
+                          |  __ \ |     | |         | |
+                          | |  \/ | ___ | |__   __ _| |
+                          | | __| |/ _ \| '_ \ / _` | |
+                          | |_\ \ | (_) | |_) | (_| | |
+                           \____/_|\___/|_.__/ \__,_|_|
+                        ______      __ _
+                        |  _  \    / _(_)
+                        | | | |___| |_ _ _ __   ___  ___
+                        | | | / _ \  _| | '_ \ / _ \/ __|
+                        | |/ /  __/ | | | | | |  __/\__ \
+                        |___/ \___|_| |_|_| |_|\___||___/
+
  * File:   GlobalDefines.h
- * Author: Tyler
+ * Author: Tyler Holmes
  *
- * Created on April 14, 2015, 10:32 AM
+ * This file is responsible for delaring various #defines that are to be used
+ * throughout the various files in this project. This includes global constants,
+ * device addresses, and inline functions. This file should contain no compiled
+ * functions or variables. 
+
+Copyright (c) 2015 Broderick Carlin & Tyler Holmes
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
 #ifndef GLOBALDEFINES_H
 #define	GLOBALDEFINES_H
 
-#ifdef	__cplusplus
-extern "C" {
-#endif
-
+// I2C addresses for various devices
 #define WII_CAM_ADR        0xB0
 #define WII_CAM_SLAVE_ADR  0x21
 #define COMPASS_ADR        0x3C
@@ -23,22 +54,17 @@ extern "C" {
 
 #define _XTAL_FREQ 64000000
 
-//Macros
 
+// Macros for various simple actions
 #define MAX(x,y)    ((x > y) ? x : y)
 #define MIN(x,y)    ((x < y) ? x : y)
 #define ABS(x)      ((x < 0) ? (x*-1) : x)
 #define bitSet(x,y) x |= (0x1 << y);
-
 
 #define disablePPSLock() IOLOCK = 0
 #define enablePPSLock() IOLOCK = 1
 #define disableInterrupts() GIE = 0
 #define enableInterrupts() GIE = 1
 
-#ifdef	__cplusplus
-}
 #endif
-
-#endif	/* GLOBALDEFINES_H */
 
